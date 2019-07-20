00feea35a03d71c06a3fdcedf43af0e6557d8c84
nip403 <41174986+nip403@users.noreply.github.com>
2019-07-20 02:54:01 +0100

    Create main.pl

---
+++ main.pl
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+
+use lib::JSONParser;
+use Data::Dumper;
+
+use feature "say";
+
+my $filename = "jsoneg.json";
+
+my $parser = new JSONParser();
+my $data = $parser->parse($filename);
+
+my @elements = ("quiz", "maths", "q2", "options");
+my $wanted = $parser->retrieve($data, \@elements);
+say Dumper $wanted;
